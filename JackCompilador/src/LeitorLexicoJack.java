76c824897c5b3ae8067d555e2bc40cc1aefb1ce4
gabryella <gabryella.cruz@discente.ufma.br>
2026-04-05 10:33:30 -0300

    inicialização, remoção de comentários e motor de tokenização
    Este commit estabelece a base do Scanner:
    - Leitura completa do arquivo .jack para memória.
    - Limpeza automática de comentários de linha (//) e de bloco (/* */).
    - Implementação do método tokenizar usando Expressões Regulares para separar símbolos, números, strings e identificadores.

--- JackCompilador/src/LeitorLexicoJack.java
+++ JackCompilador/src/LeitorLexicoJack.java
@@ -1,2 +1,38 @@
+/// ///////////////////////////////////////////////Blibliotecas///////////////////////////////////////////////
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+/// /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 public class LeitorLexicoJack {
-}
+    private String conteudo;
+    private List<String> tokens;
+    private int indiceTokenAtual;
+
+    public LeitorLexicoJack(File arquivoEntrada) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (Scanner scanner = new Scanner(arquivoEntrada)) {
+            while (scanner.hasNextLine()) {
+                sb.append(scanner.nextLine()).append("\n");
+            }
+        }
+
+        // Remove comentários logo na leitura para facilitar a tokenização
+        this.conteudo = sb.toString().replaceAll("//.*|/\\*([\\s\\S]*?)\\*/", " ");
+
+        this.tokens = new ArrayList<>();
+        this.indiceTokenAtual = -1;
+        tokenizar();
+    }
+    /// ///////////////////////////////////////////////Tokenização///////////////////////////////////////////////
+    private void tokenizar() {
+        // Esta regex separa: Strings entre aspas | Símbolos | Números | Palavras/ID
+        String regex = "\"[^\"\\n]*\"|[\\{\\}\\(\\)\\[\\]\\.,;+\\-\\*/&\\|<>=~]|\\d+|[\\w_]+";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(this.conteudo);
+
+        while (matcher.find()) {
+            tokens.add(matcher.group());
+        }
+    }
+/// /////////////////////////////////////////////////////////////////////////////////////////////////////////////
