4fb85876f0f3269dfd02a9e90f0de53f9256907a
gabryella <gabryella.cruz@discente.ufma.br>
2026-04-05 11:27:06 -0300

    Leitor pronto, pode dale na sua parte usando temMaisTokens(), avancar(), obterToken() e tipoToken()

--- JackCompilador/src/LeitorLexicoJack.java
+++ JackCompilador/src/LeitorLexicoJack.java
@@ -47,4 +47,34 @@ private static final Set<String> PALAVRAS_CHAVE = new HashSet<>(Arrays.asList(
 ));
 //símbolos válidos
 private static final String SIMBOLOS = "{}()[].,;+-*/&|<>=~";
+
+/// ///////////////////////////////////////////////Navegação e Tipos/////////////////////////////////////////
+// Verifica se ainda existem tokens na lista para processar
+public boolean temMaisTokens() {
+    return indiceTokenAtual < tokens.size() - 1;
+}
+
+// Avança o ponteiro para o próximo token da lista
+public void avancar() {
+    if (temMaisTokens()) {
+        indiceTokenAtual++;
+    }
+}
+
+// Retorna o texto do token atual
+public String obterToken() {
+    return tokens.get(indiceTokenAtual);
+}
+
+// Identifica a categoria do token para a tag XML correspondente
+public String tipoToken() {
+    String t = obterToken();
+
+    if (PALAVRAS_CHAVE.contains(t)) return "keyword";
+    if (t.length() == 1 && SIMBOLOS.contains(t)) return "symbol";
+    if (t.startsWith("\"")) return "stringConstant";
+    if (Character.isDigit(t.charAt(0))) return "integerConstant";
+    return "identifier";
+}
 /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
