0c3d48053611f57fdff39217b173380fb3373419
gabryella <gabryella.cruz@discente.ufma.br>
2026-04-20 16:56:42 -0300

    criei a função de consumir tokens e o ajuste automático de caracteres especiais no XML

--- src/Main/java/MecanismoCompilacao.java
+++ src/Main/java/MecanismoCompilacao.java
@@ -20,4 +20,30 @@ public class MecanismoCompilacao {
     public void fechar() {
         escritor.close();
     }
+    /////////////////////////////////////////////////Escrita XML///////////////////////////////////////////////
+    private void imprimirIdentacao() {
+        for (int i = 0; i < nivelIdentacao; i++) escritor.print("  ");
+    }
+
+    private void consumir(String esperado) {
+        escreverToken();
+        if (leitor.temMaisTokens()) {
+            leitor.avancar();
+        }
+    }
+
+    private void escreverToken() {
+        String tipo = leitor.tipoToken();
+        String conteudo = leitor.obterToken();
+
+        /////////////////////////////////////////////Escapamento XML///////////////////////////////////////////
+        if (conteudo.equals("<")) conteudo = "&lt;";
+        else if (conteudo.equals(">")) conteudo = "&gt;";
+        else if (conteudo.equals("&")) conteudo = "&amp;";
+        else if (conteudo.equals("\"")) conteudo = "&quot;";
+
+        imprimirIdentacao();
+        String tag = tipo.toLowerCase().replace("_", "");
+        escritor.println("<" + tag + "> " + conteudo + " </" + tag + ">");
+    }
 }
